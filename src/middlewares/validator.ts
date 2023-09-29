91b1866148cfe7ecac6d5f645f5e064152665856
awais512 <raza.awais25@gmail.com>
2023-09-29 17:28:41 +0100

    Code Refactor

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -1,10 +1,10 @@
 import * as yup from "yup";
 import { RequestHandler } from "express";
-import { CreateUserSchema } from "@/utils/validationSchema";
 
 export const validate = (schema: any): RequestHandler => {
   return async (req, res, next) => {
-    if (!req.body) return res.json({ error: "Empty body is not accepted" });
+    if (!req.body)
+      return res.status(422).json({ error: "Empty body is not accepted" });
 
     const schemaToValidate = yup.object({
       body: schema,
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,21 +1,10 @@
-import { CreateUser } from "@/@types/user";
+import { create } from "@/controllers/user";
 import { validate } from "@/middlewares/validator";
-import User from "@/models/user";
 import { CreateUserSchema } from "@/utils/validationSchema";
 import express from "express";
 
 const router = express.Router();
 
-router.post(
-  "/create",
-  validate(CreateUserSchema),
-  async (req: CreateUser, res) => {
-    const { email, password, name } = req.body;
-    CreateUserSchema.validate({ email, password, name }).catch((error) => {});
-
-    const user = await User.create({ name, email, password });
-    res.json({ user });
-  }
-);
+router.post("/create", validate(CreateUserSchema), create);
 
 export default router;
