06b32a9e93663de2a08a9fc7cf97e58aefb53ab1
awais512 <raza.awais25@gmail.com>
2023-10-05 22:54:03 +0100

    Verifying Password Reset Token

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,21 +1,27 @@
 import {
   create,
   generateForwardPasswordLink,
+  isValidPasswordResetToken,
   sendReVerificationToken,
   verifyEmail,
 } from "@/controllers/user";
 import { validate } from "@/middlewares/validator";
 import {
   CreateUserSchema,
-  EmailVerificationBody,
+  TokenAndIDValidation,
 } from "@/utils/validationSchema";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/create", validate(CreateUserSchema), create);
-router.post("/verify-email", validate(EmailVerificationBody), verifyEmail);
+router.post("/verify-email", validate(TokenAndIDValidation), verifyEmail);
 router.post("/re-verify-email", sendReVerificationToken);
 router.post("/forget-password", generateForwardPasswordLink);
+router.post(
+  "/verify-pass-reset-token",
+  validate(TokenAndIDValidation),
+  isValidPasswordResetToken
+);
 
 export default router;
